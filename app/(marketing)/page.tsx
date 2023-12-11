1c1f81657860103934abc64f438aa5eb8fe0a968
Abhinav Nath <abhinavnath@ymail.com>
2023-12-11 21:50:31 +0530

    Use the local font for styling

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,23 @@
 import Link from "next/link";
+import localFont from "next/font/local";
 import { Medal } from "lucide-react";
 
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+});
 
 const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
-      <div className="flex items-center justify-center flex-col">
+      <div
+        className={cn(
+          "flex items-center justify-center flex-col",
+          headingFont.className
+        )}
+      >
         <div
           className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100
          text-amber-700 rounded-full uppercase"
