162a708eabd0e557c79ca2ac22f0eccafd0832be
Abhinav Nath <abhinavnath@ymail.com>
2023-12-11 22:14:22 +0530

    Add logo to the navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,3 +1,17 @@
+import { Logo } from "@/components/logo";
+
 export const Navbar = () => {
-  return <div>Navbar!</div>;
+  return (
+    <div
+      className="fixed top-0 w-full h-14 px-4 border-b
+  shadow-sm bg-white flex items-center"
+    >
+      <div
+        className="md:max-w-screen-2xl mx-auto flex items-center
+     w-full justify-between"
+      >
+        <Logo />
+      </div>
+    </div>
+  );
 };
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 import localFont from "next/font/local";
 
 import { cn } from "@/lib/utils";
